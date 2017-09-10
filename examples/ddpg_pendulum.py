4f6874fe68240df57945a9ddab226126b9982192
nosyndicate <nosyndicate@gmail.com>
2017-09-10 19:32:08 -0400

    experimental script for ddpg

--- examples/ddpg_pendulum.py
+++ examples/ddpg_pendulum.py
@@ -3,12 +3,19 @@ import gym
 
 from rllab.envs.gym_env import GymEnv
 from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import run_experiment_lite
+from rllab.misc.instrument import VariantGenerator, variant
 
 from pytorchrl.algos.ddpg import DDPG
 from pytorchrl.exploration_strategies.ou_strategy import OUStrategy
 from pytorchrl.policies.deterministic_mlp_policy import DeterministicMLPPolicy
 from pytorchrl.q_functions.continuous_mlp_q_function import ContinuousMLPQFunction
 
+
+class VG(VariantGenerator):
+    @variant
+    def seed(self):
+        return [1, 11, 21, 31, 41]
 
 def run_task(*_):
     env = normalize(GymEnv("Pendulum-v0", record_video=False, force_reset=True))
@@ -37,7 +44,7 @@ def run_task(*_):
         max_path_length=100,
         epoch_length=1000,
         min_pool_size=10000,
-        n_epochs=1000,
+        n_epochs=80,
         discount=0.99,
         scale_reward=0.01,
         qf_learning_rate=1e-3,
@@ -47,6 +54,21 @@ def run_task(*_):
     )
     algo.train()
 
+variants = VG().variants()
 
-if __name__ == '__main__':
-    run_task()
+for v in variants:
+    run_experiment_lite(
+        run_task,
+        exp_prefix="ddpg_pendulum",
+        # Number of parallel workers for sampling
+        n_parallel=1,
+        # Only keep the snapshot parameters for the last iteration
+        snapshot_mode="last",
+        # Specifies the seed for the experiment. If this is not provided, a random seed
+        # will be used
+        seed=v["seed"],
+        variant=v,
+        # plot=True,
+        # terminate_machine=False,
+    )
+
