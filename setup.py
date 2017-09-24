bb5ff766294fbc4b489690e92f551290038c7941
nosyndicate <nosyndicate@gmail.com>
2017-09-23 22:18:55 -0400

    update setup.py to be able to install egg through pip

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # setup.py
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pytorchrl',
     version='0.0.1',
     author='Ermo Wei',
-    packages=['pytorchrl'],
+    packages=find_packages()
 )
