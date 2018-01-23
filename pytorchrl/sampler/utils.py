94e3edc2aaa401f8e492dda8a008e7052bc3c2a1
nosyndicate <nosyndicate@gmail.com>
2018-01-23 16:25:02 -0500

    update to use pytorch 0.3

--- pytorchrl/sampler/utils.py
+++ pytorchrl/sampler/utils.py
@@ -17,6 +17,7 @@ def rollout(env, agent, max_path_length=np.inf, animated=False, speedup=1,
         env.render()
     while path_length < max_path_length:
         a, agent_info = agent.get_action(o)
+        # print(a)
         next_o, r, d, env_info = env.step(a)
         observations.append(o)
         rewards.append(r)
